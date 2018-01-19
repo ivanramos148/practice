67f925be879b581b0f58bf7c9c52f041308be54e
ivanramos148248@gmail.com <ivanramos148248@gmail.com>
2018-01-19 09:59:03 -0800

    add ifs to scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,11 @@
 
-var machine = function(robo) {
 
-
-
-
-
+};
 $(document).ready(function() {
-    $("form#beep").submit(function(event) {
+    $("#beep").submit(function(event) {
       even.preventDefault();
-      var user = parseInt($("input#user").val());
-      var result = machine(robo);
-
-    });
-));
+      var user = ("#user").val();
+      $("answer").text(newArray.join(""));
+      event.preventDefault();
+});
+});
