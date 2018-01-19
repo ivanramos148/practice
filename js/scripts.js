544ca3b565027428c55e344f41df3c937f15ca77
ivanramos148248@gmail.com <ivanramos148248@gmail.com>
2018-01-19 14:35:36 -0800

    remove ifs, user input 0 = boop working

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
-function robo(person) {
-  var beep = [0-9]
-  var boop [0]
-  if (person === "1") {
-    return "boop"
-    ('#answer').show();
-  }
+function robo(userInput) {
+
+  //Any characters A-Z will not work
+  if(userInput.match(/[1-9]/i)) {
+
+  //if user inputs the num 0 robot will replace it with BOOP
+}if (userInput === "1") {
+    userInput = "boop";
+    return userInput;
 }
 
 
 
 
 
-$(document).ready(function() {
-  $("#form").submit(function(event) {
-   event.preventDefault();
- });
-});
+
+
+
+
+
+
+  }
