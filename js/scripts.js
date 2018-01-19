cdbd1af1eee9be7a737d5999160ce1ea03e6e1a8
ivanramos148248@gmail.com <ivanramos148248@gmail.com>
2018-01-19 08:16:01 -0800

    add README.md css and scripts

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $("form#formOne").submit(function(event) {
+		$('#trail1').hide();
+		$('#trail2').hide();
+		$('#path').hide();
+
+ var plan = $("select#plan").val();
+ var exprienced = $("select#exprienced").val();
+ var passion = $("select#passion").val();
+ var year = $("select#year").val();
+ var prefer = $("select#prefer").val();
+ var dif = $("select#dif").val();
+
+ if ((dif === 'easy') && (plan === 'yes') && (exprienced === 'yes1') && (passion === 'yes2') && (year === 'yes3') && (prefer === 'college')) {
+$('#trail1').show();
+} else {
+ if ((dif === 'hard') && (plan === 'no') && (exprienced === 'no1') && (passion === 'no2') && (year === 'no3') && (prefer === 'boot')) {
+  $('#trail2').show();
+} else  {
+  if ((dif === 'na') && (plan === 'na') && (exprienced === 'na') && (passion === 'na') && (year === 'na') && (prefer === 'na')) {
+      $('#path').show(); {};
+         }
+       }
+     }
+   });
+});
