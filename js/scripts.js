86607004de903e0a632cf58219100ad834932fd4
ivanramos148248@gmail.com <ivanramos148248@gmail.com>
2018-01-19 15:20:34 -0800

    add 1,3 to boop

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,38 @@
-function robo(userInput) {
+function piglatin(userInput) {
 
-  //Any characters A-Z will not work
-  if(userInput.match(/[1-9]/i)) {
+  //Any characters from A-Z cannot communicate with the robot
+  if (userInput.match(/[1-9]/i)) {
 
-  //if user inputs the num 0 robot will replace it with BOOP
-}if (userInput === "1") {
-    userInput = "boop";
+    //if user inputs thge 1 the out come will be boop
+  }if (userInput === "1") {
+      userInput = "boop";
+      return userInput;
+
+    //if the first letter constant piglatinify it
+  }  else if (userInput === "0") {
+    userInput = "beep"
     return userInput;
-}
-
-
-
-
-
-
-
-
-
-
-
   }
+
+    //Robot doesnt like that action
+     else if (userInput % 3 === 0) {
+       userInput = "I'm sorry, Dave. I'm afraid I can't do that"
+       return userInput
+
+      }
+   }
+$(document).ready(function() {
+  var newArray = [];
+  $("#userInput").submit(function(event) {
+    var userInput1 = $("#userInput1").val().toLowerCase().split(/\b/);
+    for (i=0; i<userInput1.length; i++) {
+      newArray.push(piglatin(userInput1[i]));
+    }
+
+    //the beep boops will pop-up
+
+    $("#answers").text(newArray.join(""));
+      event.preventDefault();
+
+    });
+});
