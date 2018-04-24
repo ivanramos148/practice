1161abbd34eab07280da6bfdd77aa0abf898d105
ivanramos148248@gmail.com <ivanramos148248@gmail.com>
2018-04-24 16:38:51 -0700

    finished project

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
+//Business Logic
 function diceRoll() {
   return Math.floor(Math.random() * 6 + 1);
 }
-
-function Player() {
+function Player(name) {
+  this.name = name;
   this.roll = 0;
   this.currentScore = 0;
-  this.permaScore = 0;
+  this.total = 0;
 }
 Player.prototype.playerRoll = function() {
   if (this.roll == 1) {
@@ -15,31 +16,81 @@ Player.prototype.playerRoll = function() {
   }
 }
 Player.prototype.playerHold = function() {
-  this.permaScore += this.currentScore
-  this.currentScore = 0
-  // $('#playerOneRoll').fadeOut();
+  this.total += this.currentScore;
+}
+Player.prototype.setToZero = function() {
+  this.currentScore = 0;
+  this.roll = 0;
 }
 Player.prototype.winChecker = function() {
-  if(this.permaScore >= 100){
-    alert('you win!');
+  if (this.total >= 100) {
+    alert(this.name + "wins!");
+  }
+}
+Player.prototype.PlayerOneHitsOne = function() {
+  if (this.roll == 1) {
+    this.roll = 0;
+    alert(this.name + "'s turn is over");
+    $("#playerOneRoll").fadeOut();
+    $("#playerTwoRoll").fadeIn();
+  }
+}
+Player.prototype.PlayerTwoHitsOne = function() {
+  if (this.roll == 1) {
+    this.roll = 0;
+    alert(this.name + "'s turn is over");
+    $("#playerTwoRoll").fadeOut();
+    $("#playerOneRoll").fadeIn();
   }
 }
 //User Interface
 $(document).ready(function() {
-  var playerOne = new Player
-  var playerTwo = new Player
-  $("#playerOneRoll").submit(function(event) {
-    $("#answers").empty('');
+  $('#formOne').submit(function(event) {
     event.preventDefault();
-    playerOne.roll = diceRoll();
-    playerOne.playerRoll();
-    $("#answers").append(playerOne.currentScore + ' ' + playerOne.roll)
-  });
-  $("#playerOneHold").submit(function(event) {
-    $("#playerHold").empty('')
-    event.preventDefault();
-    playerOne.playerHold();
-    playerOne.winChecker();
-    $("#playerHold").append(playerOne.permaScore)
+    var userOne = $('#player1').val();
+    var userTwo = $('#player2').val();
+    var playerOne = new Player(userOne);
+    var playerTwo = new Player(userTwo);
+    $("#playerOneName").append(playerOne.name + ':');
+    $("#playerTwoName").append(playerTwo.name + ':');
+
+    $("#playerOneRoll").submit(function(event) {
+      $("#playerOneResults").empty('');
+      event.preventDefault();
+      playerOne.roll = diceRoll();
+      playerOne.playerRoll();
+      playerOne.PlayerOneHitsOne();
+      $("#playerOneResults").append('Player score: ' + playerOne.currentScore + ' dice: ' + playerOne.roll);
+    });
+    $("#playerOneHold").submit(function(event) {
+      $("#playerOnePerma").empty('');
+      $("#playerOneRoll").fadeOut();
+      $("#playerTwoRoll").fadeIn();
+      event.preventDefault();
+      playerOne.playerHold();
+      playerOne.winChecker();
+      $("#playerOnePerma").append('total: ' + playerOne.total);
+    });
+    //
+    //PlayerTwo
+    //
+    $("#playerTwoRoll").submit(function(event) {
+      $("#playerTwoResults").empty('');
+      event.preventDefault();
+      playerTwo.roll = diceRoll();
+      playerTwo.playerRoll();
+      playerTwo.PlayerTwoHitsOne();
+      $("#playerTwoResults").append('Player score: ' + playerTwo.currentScore + ' dice: ' + playerTwo.roll);
+    });
+    //PlayerTwoHold
+    $("#playerTwoHold").submit(function(event) {
+      $("#playerTwoPerma").empty('');
+      $("#playerTwoRoll").fadeOut();
+      $("#playerOneRoll").fadeIn();
+      event.preventDefault();
+      playerTwo.playerHold();
+      playerTwo.winChecker();
+      $("#playerTwoPerma").append('total: ' + playerTwo.total);
+    });
   });
 });
